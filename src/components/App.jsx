ac37e5d914cc4580bb712ad3631e8c8b02237512
blavikensbutcher <luckertheory@gmail.com>
2024-02-12 22:50:55 +0200

    add default export

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Header } from './Header/Header.jsx';
 import { Film } from './Film/Film.jsx';
+import { Reviews } from './Reviews/Reviews.jsx';
+import { Cast } from './Cast/Cast.jsx';
 
 const FilmsList = React.lazy(() => import('../pages/FilmsList/FilmsList.jsx'));
-const Reviews = React.lazy(() => import('./Reviews/Reviews.jsx'));
-const Cast = React.lazy(() => import('./Cast/Cast.jsx'));
 const NotFoundPage = React.lazy(() => import('../pages/NotFoundPage/NotFoundPage.jsx'));
 const SearchMovies = React.lazy(() => import('../pages/SearchMovies/SearchMovies.jsx'));
 
